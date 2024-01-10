fc67de19fa060b8b915d32aa3093ed48ecb470ca
manasbekov <manasbekov_a@auca.kg>
2024-01-10 15:08:46 +0600

    Implemented modal window for log out and order cancellation

---
+++ src/components/ModalWindow.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+import "../styles/components/ModalWindow.css";
+import closeIcon from "../images/cancelIcon.svg";
+
+const ModalWindow = ({ title, message, onConfirm, onCancel }) => {
+  return (
+    <div className="modal-overlay">
+      <div className="modal-window">
+        <div className="modal-header">
+          <h2>{title}</h2>
+          <button className="modal-close-button" onClick={onCancel}>
+            <img src={closeIcon} alt="" className="modal-close-icon" />
+          </button>
+        </div>
+        <div className="modal-content">
+          <p>{message}</p>
+        </div>
+        <div className="modal-actions">
+          <button className="modal-confirm-button" onClick={onConfirm}>
+            Да
+          </button>
+          <button className="modal-cancel-button" onClick={onCancel}>
+            Нет
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ModalWindow;
--- src/pages/main_subpages/profile_subpages/PersonalData.jsx
+++ src/pages/main_subpages/profile_subpages/PersonalData.jsx
@@ -1,12 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
 import { useSelector } from "react-redux";
+import { useNavigate } from "react-router-dom";
+import ModalWindow from "../../../components/ModalWindow";
 import "../../../styles/pages/main_subpages/profile_page.css";
 import signOutIcon from "../../../images/sign-out-icon.svg";
 
 const PersonalData = () => {
+  const [showModal, setShowModal] = useState(false);
+  const navigate = useNavigate();
   const personalData = useSelector((state) => state.profile);
 
-  const handleExit = () => {};
+  const handleExit = () => {
+    setShowModal(true);
+  };
+
+  const handleConfirmExit = () => {
+    navigate("/");
+  };
+
+  const handleCancelExit = () => {
+    setShowModal(false);
+  };
 
   return (
     <div className="personal-data-page">
@@ -43,9 +57,18 @@ const PersonalData = () => {
           </div>
         </div>
         <button onClick={handleExit} className="exit-button">
-          <img src={signOutIcon} alt="" className="sign-out-icon" />
+          <img src={signOutIcon} alt="Exit" className="sign-out-icon" />
           Выход
         </button>
+
+        {showModal && (
+          <ModalWindow
+            title="Выход из учетной записи"
+            message="Вы действительно хотите выйти из учетной записи?"
+            onConfirm={handleConfirmExit}
+            onCancel={handleCancelExit}
+          />
+        )}
       </div>
     </div>
   );
